3696f2d2a3239df765d9dc3a7f5a3871ab9c1e67
Asinsa <asinsa.g@gmail.com>
2022-02-24 18:06:23 +0000

    Project class

---
+++ src/Project.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class Project {
+    public String name;
+    public int duration;
+    public int score;
+    public int bestBefore;
+    public int roles;
+    public HashSet<Person> people;
+
+    public Project(String name, int duration, int score, int bestBefore, int roles) {
+        this.name = name;
+        this.duration = duration;
+        this.score = score;
+        this.bestBefore = bestBefore;
+        this.roles = roles;
+    }
+
+    public void addPerson(Person person) {
+        people.add(person);
+    }
+}

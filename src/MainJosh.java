ce5607756f3004c1a7da5e25baaef300d5e4f48c
Neil Woodhouse <clonesupreme@gmail.com>
2022-02-24 19:53:15 +0000

    Output v0.000000000000001

--- src/MainJosh.java
+++ src/MainJosh.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Array;
 
 public class MainJosh {
@@ -9,23 +13,41 @@ public class MainJosh {
         //Parser parse = new Parser("src/input/d_dense_schedule.in.txt");
         //Parser parse = new Parser("src/input/f_find_great_mentors.in.txt");
 
-        for (Project project : parse.projects) {
-            //System.out.println(project.name);
-            for (ReqSkill requiredSkills : project.reqSkills) {
-                for (Person person : parse.people) {
-                    //String[] skill = person.skills.containsKey().split("=");
-                    //skill=num
-                    //skill
-                    if (person.skills.containsKey(requiredSkills.name)) {
-                        System.out.println();
-                        if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
-                            System.out.println(project.name + person.name + requiredSkills.name);
-                            person.learn(requiredSkills.name);
-                            break;
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter("src/output/output.txt"));
+            StringBuilder outputString = new StringBuilder();
+            int completedProjects = 0;
+
+            for (Project project : parse.projects) {
+                //System.out.println(project.name);
+                StringBuilder members = new StringBuilder();
+                int rolesToFill = project.roles;
+                for (ReqSkill requiredSkills : project.reqSkills) {
+                    for (Person person : parse.people) {
+                        //String[] skill = person.skills.containsKey().split("=");
+                        //skill=num
+                        //skill
+                        if (person.skills.containsKey(requiredSkills.name)) {
+    //                        System.out.println();
+                            if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
+    //                            System.out.println(project.name + person.name + requiredSkills.name);
+                                members.append(person.name+" ");
+                                rolesToFill--;
+                                person.learn(requiredSkills.name);
+                                break;
+                            }
                         }
                     }
                 }
+                if (rolesToFill == 0) {
+                    completedProjects++;
+                    outputString.append(project.name + "\n" + members.toString() + "\n");
+                }
             }
+            printWriter.print(completedProjects + "\n" + outputString.toString());
+            printWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
 

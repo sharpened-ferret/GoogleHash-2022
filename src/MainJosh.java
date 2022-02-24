c0239be0899957ef0457f3689475ccae3c64bdba
Joshua Murray <j.murray567@gmail.com>
2022-02-24 19:34:08 +0000

    Update MainJosh.java

--- src/MainJosh.java
+++ src/MainJosh.java
@@ -9,27 +9,25 @@ public class MainJosh {
         //Parser parse = new Parser("src/input/d_dense_schedule.in.txt");
         //Parser parse = new Parser("src/input/f_find_great_mentors.in.txt");
 
-        while(!parse.projects.isEmpty()) {
-            for (Project project : parse.projects) {
-                //System.out.println(project.name);
-                for (ReqSkill requiredSkills : project.reqSkills) {
-                    for (Person person : parse.people) {
-                        //String[] skill = person.skills.containsKey().split("=");
-                        //skill=num
-                        //skill
-                        if (person.skills.containsKey(requiredSkills.name)) {
-                            System.out.println();
-                            if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
-                                System.out.println(project.name + person.name + requiredSkills.name);
-                                person.learn(requiredSkills.name);
-                                parse.projects.remove(project);
-                                break;
-                            }
+        for (Project project : parse.projects) {
+            //System.out.println(project.name);
+            for (ReqSkill requiredSkills : project.reqSkills) {
+                for (Person person : parse.people) {
+                    //String[] skill = person.skills.containsKey().split("=");
+                    //skill=num
+                    //skill
+                    if (person.skills.containsKey(requiredSkills.name)) {
+                        System.out.println();
+                        if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
+                            System.out.println(project.name + person.name + requiredSkills.name);
+                            person.learn(requiredSkills.name);
+                            break;
                         }
                     }
                 }
             }
         }
+
 
 
     }

73fa3b74479e749d2ebd5817980bf4f5fdd415cb
Alfie Richardson <01richardsonalfie@protonmail.com>
2022-02-24 18:51:50 +0000

    added all file reads to main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    Parser parse = new Parser("src/input/a_an_example.in.txt");
+        //Parser parse = new Parser("src/input/b_better_start_small.in.txt");
+        //Parser parse = new Parser("src/input/c_collaboration.in.txt");
+        //Parser parse = new Parser("src/input/d_dense_schedule.in.txt");
+        //Parser parse = new Parser("src/input/f_find_great_mentors.in.txt");
     }
 }

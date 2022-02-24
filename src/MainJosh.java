fc3c4cbf0440c53fd87982e5a763c5e82e2c68cb
Joshua Murray <j.murray567@gmail.com>
2022-02-24 20:10:02 +0000

    Working example

--- src/MainJosh.java
+++ src/MainJosh.java
@@ -3,12 +3,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainJosh {
 
     public static void main(String[] args) {
         String[] inputFilepath = {"a_an_example", "b_better_start_small", "c_collaboration", "d_dense_schedule", "e_exceptional_skills", "f_find_great_mentors"};
-
+        ArrayList<Person> inUsePerson;
         for (int i = 0; i < inputFilepath.length; i++) {
             Parser parse = new Parser("src/input/" + inputFilepath[i] + ".in.txt");
 
@@ -18,6 +19,7 @@ public class MainJosh {
                 int completedProjects = 0;
 
                 for (Project project : parse.projects) {
+                    inUsePerson = new ArrayList<>();
                     //System.out.println(project.name);
                     StringBuilder members = new StringBuilder();
                     int rolesToFill = project.roles;
@@ -26,13 +28,16 @@ public class MainJosh {
                             //String[] skill = person.skills.containsKey().split("=");
                             //skill=num
                             //skill
-                            if (person.skills.containsKey(requiredSkills.name)) {
+                            if (!(inUsePerson.contains(person)) && (person.skills.containsKey(requiredSkills.name))) {
                                 //                        System.out.println();
                                 if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
                                     //                            System.out.println(project.name + person.name + requiredSkills.name);
                                     members.append(person.name + " ");
                                     rolesToFill--;
-                                    person.learn(requiredSkills.name);
+                                    inUsePerson.add(person);
+//                                    if (person.skills.get(requiredSkills.name) <= requiredSkills.level){
+//                                        person.learn(requiredSkills.name);
+//                                    }
                                     break;
                                 }
                             }

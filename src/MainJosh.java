9c56c574eff83289193b3b7ccadbe95e20484625
Asinsa <asinsa.g@gmail.com>
2022-02-24 19:57:13 +0000

    Loops through every file

--- src/MainJosh.java
+++ src/MainJosh.java
@@ -7,50 +7,49 @@ import java.lang.reflect.Array;
 public class MainJosh {
 
     public static void main(String[] args) {
-        Parser parse = new Parser("src/input/a_an_example.in.txt");
-        //Parser parse = new Parser("src/input/b_better_start_small.in.txt");
-        //Parser parse = new Parser("src/input/c_collaboration.in.txt");
-        //Parser parse = new Parser("src/input/d_dense_schedule.in.txt");
-        //Parser parse = new Parser("src/input/f_find_great_mentors.in.txt");
+        String[] inputFilepath = {"a_an_example", "b_better_start_small", "c_collaboration", "d_dense_schedule", "e_exceptional_skills", "f_find_great_mentors"};
 
-        try {
-            PrintWriter printWriter = new PrintWriter(new FileWriter("src/output/output.txt"));
-            StringBuilder outputString = new StringBuilder();
-            int completedProjects = 0;
+        for (int i = 0; i < inputFilepath.length; i++) {
+            Parser parse = new Parser("src/input/" + inputFilepath[i] + ".in.txt");
 
-            for (Project project : parse.projects) {
-                //System.out.println(project.name);
-                StringBuilder members = new StringBuilder();
-                int rolesToFill = project.roles;
-                for (ReqSkill requiredSkills : project.reqSkills) {
-                    for (Person person : parse.people) {
-                        //String[] skill = person.skills.containsKey().split("=");
-                        //skill=num
-                        //skill
-                        if (person.skills.containsKey(requiredSkills.name)) {
-    //                        System.out.println();
-                            if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
-    //                            System.out.println(project.name + person.name + requiredSkills.name);
-                                members.append(person.name+" ");
-                                rolesToFill--;
-                                person.learn(requiredSkills.name);
-                                break;
+            try {
+                PrintWriter printWriter = new PrintWriter(new FileWriter("src/output/" + inputFilepath[i] + ".out.txt"));
+                StringBuilder outputString = new StringBuilder();
+                int completedProjects = 0;
+
+                for (Project project : parse.projects) {
+                    //System.out.println(project.name);
+                    StringBuilder members = new StringBuilder();
+                    int rolesToFill = project.roles;
+                    for (ReqSkill requiredSkills : project.reqSkills) {
+                        for (Person person : parse.people) {
+                            //String[] skill = person.skills.containsKey().split("=");
+                            //skill=num
+                            //skill
+                            if (person.skills.containsKey(requiredSkills.name)) {
+                                //                        System.out.println();
+                                if (person.skills.get(requiredSkills.name) >= requiredSkills.level) {
+                                    //                            System.out.println(project.name + person.name + requiredSkills.name);
+                                    members.append(person.name + " ");
+                                    rolesToFill--;
+                                    person.learn(requiredSkills.name);
+                                    break;
+                                }
                             }
                         }
                     }
+                    if (rolesToFill == 0) {
+                        completedProjects++;
+                        outputString.append(project.name + "\n" + members.toString() + "\n");
+                    }
                 }
-                if (rolesToFill == 0) {
-                    completedProjects++;
-                    outputString.append(project.name + "\n" + members.toString() + "\n");
-                }
+                printWriter.print(completedProjects + "\n" + outputString.toString());
+                printWriter.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-            printWriter.print(completedProjects + "\n" + outputString.toString());
-            printWriter.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+
+
         }
-
-
-
     }
 }
